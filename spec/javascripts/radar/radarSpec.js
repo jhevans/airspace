e1bcf79f628e7300fd6ed1c7e2fe543003bf2952
JohnH.Evans <johnh.evans@autotrader.co.uk>
2014-10-08 09:58:44 +0100

    Added jasmine test structure and very rough spec for radar

---
+++ spec/javascripts/radar/radarSpec.js
@@ -0,0 +1,31 @@
+/**
+ * Created by JohnH.Evans on 07/10/2014.
+ */
+
+describe('A radar graph which plots points around an axis', function () {
+    it('should create 3 concentric circles', function () {
+
+    });
+
+    it('should create a number of Blips for each of its data points', function () {
+
+    });
+});
+
+describe('A Blip, representing a point on a radar with polar coordinates', function () {
+    it('should have a radial distance coordinate', function () {
+
+    });
+
+    it("should have an angular coordinate", function () {
+
+    });
+});
+
+describe('A radial field which abstracts the properties of the drawing space', function () {
+    it('should have a radius', function () {
+
+    });
+
+
+})
